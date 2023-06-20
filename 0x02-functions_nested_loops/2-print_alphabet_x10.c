5cb422f84f6045a7ede195e48bc5fd1703871bb6
Your Name <you@example.com>
2023-06-20 12:07:17 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  *print_alphabet_x10 - printing the alphabets 1o times
+  *Return: always 0
+  */
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar('i');
+		}
+		_putchar('\n');
+	}
+}
