38afe49a1977c799507295c01a961ca4c3620942
Your Name <you@example.com>
2023-06-20 14:49:18 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  *print_sign - printing the sign of a number
+  *@x: the int to check
+  *Return: 1 if positive -1 if negative 0 is null
+  */
+int print_sign(int x)
+{
+	if (x > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (x == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (x < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
