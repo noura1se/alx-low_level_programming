f57b27472e39d5fcc808bc083a3954a97e814369
Your Name <you@example.com>
2023-06-19 17:25:30 -0700

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+  *main - the numbers of base 16
+  *Return: always 0
+  */
+
+int main(void)
+{
+	int i;
+	char x;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (x = 'a'; i >= 'f'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
