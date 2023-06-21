f1b11a98dd08fb60672faada12b1938a7bf06551
Your Name <you@example.com>
2023-06-20 17:11:43 -0700

    functions, nested loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  *jack_bauer - printing every minute of jack bauer
+  */
+
+	void jack_bauer(void)
+	{
+		int x, y;
+
+		x = 0;
+
+		while (x < 24)
+		{
+			y = 0;
+			while (y < 60)
+			{
+				_putchar((x / 10) + '0');
+				_putchar((x % 10) + '0');
+				_putchar(':');
+				_putchar((y / 10) + '0');
+				_putchar((y % 10) + '0');
+				_putchar('\n');
+				y++;
+			}
+			x++;
+		}
+	}
+
