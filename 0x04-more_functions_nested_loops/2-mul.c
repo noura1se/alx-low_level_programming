c3414932bb96e899ae5092ca504c2b4967057ea6
Your Name <you@example.com>
2023-06-22 17:09:20 -0700

    more functions, nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  *mul - multiplies two integers
+  *@a: an int to multiplies
+  *@b: an int to multiplies
+  *Return: a * b
+  */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
