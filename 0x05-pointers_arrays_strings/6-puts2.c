e1d75ddf141fc7f1219405a100b0545c1143653c
Your Name <you@example.com>
2023-06-26 15:40:45 -0700

    pointers_arrays_strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,15 +11,20 @@ void puts2(char *str)
 	int cpt;
 	int i;
 	char *x = str;
+	int y = 0; 
 
 	while (*x != '\0')
 	{
 		cpt++;
 		x++;
 	}
-	for (i = 0; i < cpt; i + 2)
+	y = cpt - 1;
+	for (i = 0; i <= y; i++)
 	{
-		_putchar(str[i]);
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }
