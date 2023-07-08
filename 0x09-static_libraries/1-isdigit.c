77332c991dfea440f1d5143d42c24bdf99cc1408
Your Name <you@example.com>
2023-07-07 17:00:00 -0700

    static libraries

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  *_isdigit - to check if c a digit or not
+  *@c: the int to check
+  *Return: 1 if c is a digit 0 if not
+  */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
